a8a040bcc2d94018f7ef9fc8f2998f20cfa4da15
Taras Voinarovskiy <voyn1991@gmail.com>
2017-05-09 10:05:12 +0000

    Moved all errors and structs to be accessible from aiokafka namespace, without importing from kafka namespace.

--- aiokafka/errors.py
+++ aiokafka/errors.py
@@ -1,5 +1,37 @@
+from kafka.errors import *  # noqa
+from kafka.errors import KafkaError
+
+__all__ = [
+    # kafka-python errors
+    "KafkaError", "ConnectionError", "NodeNotReadyError",
+    "KafkaTimeoutError", "UnknownTopicOrPartitionError",
+    "UnrecognizedBrokerVersion", "NotLeaderForPartitionError",
+    "LeaderNotAvailableError", "TopicAuthorizationFailedError",
+    "OffsetOutOfRangeError", "MessageSizeTooLargeError", "for_code", "NoError",
+    "StaleMetadata", "CorrelationIdError", "NoBrokersAvailable",
+    "RebalanceInProgressError", "IllegalGenerationError",
+    "UnknownMemberIdError", "GroupLoadInProgressError",
+    "GroupCoordinatorNotAvailableError", "NotCoordinatorForGroupError",
+    "GroupAuthorizationFailedError",
+    # aiokafka custom errors
+    "ConsumerStoppedError", "NoOffsetForPartitionError", "RecordTooLargeError",
+    "ProducerClosed"
+]
+
 
 class ConsumerStoppedError(Exception):
     """ Raised on `get*` methods of Consumer if it's cancelled, even pending
         ones.
     """
+
+
+class NoOffsetForPartitionError(KafkaError):
+    pass
+
+
+class RecordTooLargeError(KafkaError):
+    pass
+
+
+class ProducerClosed(KafkaError):
+    pass
